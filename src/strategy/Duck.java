8ee51e8e731a0e999c1ef14d97e89a2664022bc2
Virginia Chen <virginia.chen32@gmail.com>
2018-11-04 12:56:45 -0800

    Complete strategy pattern

--- src/strategy/Duck.java
+++ src/strategy/Duck.java
@@ -22,4 +22,12 @@ public abstract class Duck {
         System.out.println("All ducks float,even decoys!");
     }
 
+    public void setFlyBehavior(FlyBehavior fb) {
+        flyBehavior = fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb) {
+        quackBehavior = qb;
+    }
+
 }
---
+++ src/strategy/ModelDuck.java
@@ -0,0 +1,12 @@
+package strategy;
+
+public class ModelDuck extends Duck {
+    public ModelDuck() {
+        quackBehavior = new Quack();
+        flyBehavior = new FlyNoWay();
+    }
+
+    public void display() {
+            System.out.println("I'm a real Mallard duck");
+        }
+}
